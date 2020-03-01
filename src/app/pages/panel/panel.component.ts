637765656f88eea313c5f888993e74e17531bf8e
ISProdanov <iprodanov666@abv.bg>
2020-03-01 11:35:01 +0200

    added logout & show user details

--- src/app/pages/panel/panel.component.ts
+++ src/app/pages/panel/panel.component.ts
@@ -1,18 +1,48 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Data, Router} from '@angular/router';
 
+import {Subscription} from 'rxjs';
+
+import {UserModel} from '../../shared/models';
 import {PanelPathsEnum} from '../../shared/enums';
+import {AuthService} from '../../shared/services';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.scss']
 })
-export class PanelComponent implements OnInit {
-  public panelPathsEnum = PanelPathsEnum;
+export class PanelComponent implements OnInit, OnDestroy {
+  private activatedRouteSubscription: Subscription;
 
-  constructor() {
+  public panelPathsEnum = PanelPathsEnum;
+  public user: UserModel;
+
+  constructor(
+    private  router: Router,
+    private  activateRoute: ActivatedRoute,
+    private  authService: AuthService
+  ) {
   }
 
   ngOnInit() {
+    this.activateRoute.data.subscribe((data: Data) => {
+      if (!data) {
+        this.router.navigate(['/error'])
+          .finally();
+      }
+
+      this.user = data.data;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.activatedRouteSubscription) {
+      this.activatedRouteSubscription.unsubscribe();
+    }
+  }
+
+  onLogout(): void {
+    this.authService.signOut();
   }
 }
