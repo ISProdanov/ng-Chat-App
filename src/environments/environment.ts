3f722b974fb55135328277ce4a4327dd476df6f5
ISProdanov <53855865+ISProdanov@users.noreply.github.com>
2020-04-05 11:15:21 +0300

    removed firebase keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,16 +9,6 @@ export const environment = {
   production: false,
 
   currentTimeLong: moment().format('HH:mm:ss DD.MM.YYYY'),
-
-  firebaseConfig: {
-    apiKey: 'AIzaSyBmuODKz4zngywdwjle2yZYT7fmNIVMxHw',
-    authDomain: 'ng-chat-app-c55d7.firebaseapp.com',
-    databaseURL: 'https://ng-chat-app-c55d7.firebaseio.com',
-    projectId: 'ng-chat-app-c55d7',
-    storageBucket: 'ng-chat-app-c55d7.appspot.com',
-    messagingSenderId: '349736771990',
-    appId: '1:349736771990:web:0ead0968eef47fa8ba3db7'
-  },
 };
 
 /*
